f2cab49cef61893d60bfba4c9309d61782328a03
Daniela Vornic <daniela.vornic@gmail.com>
2022-04-28 15:52:47 +0300

    make login pages and register pages jsx.elements instead of FC

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,22 +1,19 @@
-import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 import LoginCard from '../components/LoginCard';
 
 import '../../../styles/AuthPage.scss';
 
-const Login: FC = () => {
-  return (
-    <div className='page-container'>
-      <div className='content'>
-        <h1>Welcome back to EBS Dashboard</h1>
-        <LoginCard />
-        <p className='switch-auth'>
-          Don't have an account yet? <Link to={'/register'}>Register</Link>
-        </p>
-      </div>
+const Login = () => (
+  <div className='page-container'>
+    <div className='content'>
+      <h1>Welcome back to EBS Dashboard</h1>
+      <LoginCard />
+      <p className='switch-auth'>
+        Don't have an account yet? <Link to={'/register'}>Register</Link>
+      </p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Login;
